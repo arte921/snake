fdbc2bec456c1aabe4aaf400d73aae38e0682f6b
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-06 14:04:49 +0100

    d

--- main.js
+++ main.js
@@ -5,3 +5,31 @@ var canvas = document.getElementById('canvas');
 canvas.width = mcbwidth;
 canvas.height = mcbheight;
 var ctx = canvas.getContext('2d');
+
+var gridsizefactor = 20;
+
+var xt = Math.floor(mcbwidth/gridsizefactor);
+var yt = Math.floor(mcbheight/gridsizefactor);
+
+var grid = new Array(xt).fill(new Array(yt).fill(0));
+var items = new Array(xt).fill(new Array(yt).fill(0));
+
+var length = 3;
+
+var total = xt*yt;
+
+items[10][10] = 1;
+
+function render(){
+  items.forEach(function(element,x){
+    element.forEach(function(element,y){
+      console.log(x,y);
+
+    });
+  });
+}
+
+
+
+render();
+console.log(total);
