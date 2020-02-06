c525d30ae4286b7dfcf722ac4ef4cca8bc72d578
Ard-Jan van Etten <arte@linux.fritz.box>
2020-02-06 13:39:48 +0100

    canvas imp

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('hello worlds');
+var mcbwidth = document.body.clientWidth;
+var mcbheight = document.body.clientHeight;
+
+var canvas = document.getElementById('canvas');
+canvas.width = mcbwidth;
+canvas.height = mcbheight;
+var ctx = canvas.getContext('2d');
